05319d7dfb15dd67c3233256bb97cfb0028656e1
williamsoro <william.soro@hotmail.it>
2016-05-03 03:18:26 +0200

    From Missing to Present Creatures entries: 1,2,3,6,30,36,38,40,43,46,48,54,60,61,66,68,69,74,78,79,80,89,92,94,95,97,98,99

--- Translations/Italian/Italian_Creature.sql
+++ Translations/Italian/Italian_Creature.sql
@@ -1,3 +1,30 @@
 SET NAMES 'utf8';
-
+UPDATE locales_creature SET name_loc9='Waypoint',subname_loc9='GM Visual' WHERE entry=1; -- Name: Waypoint -- subName: GM Visual
+UPDATE locales_creature SET name_loc9='Spawnpoint',subname_loc9='GM Visual' WHERE entry=2; -- Name: Spawnpoint -- subName: GM Visual
+UPDATE locales_creature SET name_loc9='Mangiatore di Carne',subname_loc9='' WHERE entry=3; -- Name: Flesh Eater
+UPDATE locales_creature SET name_loc9='Coboldo Predatore',subname_loc9='' WHERE entry=6; -- Name: Kobold Vermin
+UPDATE locales_creature SET name_loc9='Ragno della Foresta',subname_loc9='' WHERE entry=30; -- Name: Forest Spider
+UPDATE locales_creature SET name_loc9='Golem del Raccolto',subname_loc9='' WHERE entry=36; -- Name: Harvest Golem
+UPDATE locales_creature SET name_loc9='Malvivente Defias',subname_loc9='' WHERE entry=38; -- Name: Defias Thug
+UPDATE locales_creature SET name_loc9='Coboldo Minatore',subname_loc9='' WHERE entry=40; -- Name: Kobold Miner
+UPDATE locales_creature SET name_loc9='Ragno delle Miniere',subname_loc9='' WHERE entry=43; -- Name: Mine Spider
+UPDATE locales_creature SET name_loc9='Murloc Depredatore',subname_loc9='' WHERE entry=46; -- Name: Murloc Forager
+UPDATE locales_creature SET name_loc9='Guerriero Scheletrico',subname_loc9='' WHERE entry=48; -- Name: Skeletal Warrior
+UPDATE locales_creature SET name_loc9='Corina Steele',subname_loc9='Fabbro D\'Armi' WHERE entry=54; -- Name: Corina Steele -- subName: Fabbro D\'Armi
+UPDATE locales_creature SET name_loc9='Ruklar il Cacciatore',subname_loc9='' WHERE entry=60; -- Name: Ruklar the Trapper
+UPDATE locales_creature SET name_loc9='Thuros Lightfingers',subname_loc9='' WHERE entry=61; -- Name: Thuros Lightfingers
+UPDATE locales_creature SET name_loc9='Tharynn Bouden',subname_loc9='Mercante di Forniture Commerciali' WHERE entry=66; -- Name: Tharynn Bouden -- subName: Mercante di Forniture Commerciali
+UPDATE locales_creature SET name_loc9='Guardi di Stormwind',subname_loc9='' WHERE entry=68; -- Name: Stormwind City Guard
+UPDATE locales_creature SET name_loc9='Lupo della Foresta',subname_loc9='' WHERE entry=69; -- Name: Timber Wolf
+UPDATE locales_creature SET name_loc9='Kurran Steele',subname_loc9='Mercante di Armature in Stoffa e Pelle' WHERE entry=74; -- Name: Kurran Steele -- subName: Mercante di Armature in Stoffa e Pelle
+UPDATE locales_creature SET name_loc9='Janos Hammerknuckle',subname_loc9='Fabbro D\'Armi' WHERE entry=78; -- Name: Janos Hammerknuckle -- subName: Fabbro D\'Armi
+UPDATE locales_creature SET name_loc9='Caposquadra Narg',subname_loc9='' WHERE entry=79; -- Name: Narg the Taskmaster
+UPDATE locales_creature SET name_loc9='Kobold Operaio',subname_loc9='' WHERE entry=80; -- Name: Kobold Laborer
+UPDATE locales_creature SET name_loc9='Demone Infernale',subname_loc9='' WHERE entry=89; -- Name: Infernal
+UPDATE locales_creature SET name_loc9='Elementale di Pietra',subname_loc9='' WHERE entry=92; -- Name: Rock Elemental
+UPDATE locales_creature SET name_loc9='Tagliaborse Defias',subname_loc9='' WHERE entry=94; -- Name: Defias Cutpurse
+UPDATE locales_creature SET name_loc9='Contrabbandiere Defias',subname_loc9='' WHERE entry=95; -- Name: Defias Smuggler
+UPDATE locales_creature SET name_loc9='Giovane Riverpaw',subname_loc9='' WHERE entry=97; -- Name: Riverpaw Runt
+UPDATE locales_creature SET name_loc9='Caposquadra Riverpaw',subname_loc9='' WHERE entry=98; -- Name: Riverpaw Taskmaster
+UPDATE locales_creature SET name_loc9='Morgaine l\'Astuta',subname_loc9='' WHERE entry=99; -- Name: Morgaine the Sly
 SET NAMES 'latin1';
