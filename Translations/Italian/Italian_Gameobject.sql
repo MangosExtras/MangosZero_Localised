5dc3db489953feb2a8ac4d2076545371c61ea59b
gabryatfendor <gabrielesani@programmer.net>
2016-05-09 08:59:10 +0200

    Finished translating GameObjects in Italian

--- Translations/Italian/Italian_Gameobject.sql
+++ Translations/Italian/Italian_Gameobject.sql
@@ -10281,4 +10281,146 @@ UPDATE locales_gameobject SET name_loc9='Fumo Calderone' WHERE entry=190395; -- 
 UPDATE locales_gameobject SET name_loc9='Fumo Boccale' WHERE entry=190396; -- Name: Mug Smoke
 UPDATE locales_gameobject SET name_loc9='Forziere Documenti' WHERE entry=190483; -- Name: Document Chest
 UPDATE locales_gameobject SET name_loc9='Forziere Documenti' WHERE entry=190484; -- Name: Document Chest
+UPDATE locales_gameobject SET name_loc9='Cannone' WHERE entry=193011; -- Name: Cannon
+UPDATE locales_gameobject SET name_loc9='Area Personalizzata Accampamento Fel Orc' WHERE entry=200000; -- Name: Fel Orc Encampment Custom Area
+UPDATE locales_gameobject SET name_loc9='Mercato' WHERE entry=200001; -- Name: Shopping Centre
+UPDATE locales_gameobject SET name_loc9='Stendardo Pendente Capodanno Alleanza' WHERE entry=210210; -- Name: New Year Alliance Hanging Banner
+UPDATE locales_gameobject SET name_loc9='Stendardo Pendente Capodanno Alleanza 02' WHERE entry=210211; -- Name: New Year Alliance Hanging Banner 02
+UPDATE locales_gameobject SET name_loc9='Stendardo Pendente Capodanno Orda' WHERE entry=210212; -- Name: New Year Horde Hanging Banner
+UPDATE locales_gameobject SET name_loc9='Stendardo Pendente Capodanno Orda 02' WHERE entry=210213; -- Name: New Year Horde Hanging Banner 02
+UPDATE locales_gameobject SET name_loc9='Stendardo Capodanno Alleanza' WHERE entry=210214; -- Name: New Year Alliance Standing Banner
+UPDATE locales_gameobject SET name_loc9='Stendardo Capodanno Orda' WHERE entry=210215; -- Name: New Year Horde Standing Banner
+UPDATE locales_gameobject SET name_loc9='Padiglione degli Scavi' WHERE entry=210286; -- Name: Excavation Tent Pavillion
+UPDATE locales_gameobject SET name_loc9='Cristallo Ossirian di Ahn\'Qiraj' WHERE entry=210312; -- Name: Ahn'Qiraj Ossirian Crystal
+UPDATE locales_gameobject SET name_loc9='Cristallo Ossirian di Ahn\'Qiraj' WHERE entry=210313; -- Name: Ahn'Qiraj Ossirian Crystal
+UPDATE locales_gameobject SET name_loc9='Cristallo di Comunicazione' WHERE entry=210327; -- Name: Communication Crystal
+UPDATE locales_gameobject SET name_loc9='Cristallo di Comunicazione' WHERE entry=210328; -- Name: Communication Crystal
+UPDATE locales_gameobject SET name_loc9='Cristallo di Comunicazione' WHERE entry=210329; -- Name: Communication Crystal
+UPDATE locales_gameobject SET name_loc9='Cristallo di Comunicazione' WHERE entry=210330; -- Name: Communication Crystal
+UPDATE locales_gameobject SET name_loc9='Cristallo di Comunicazione' WHERE entry=210331; -- Name: Communication Crystal
+UPDATE locales_gameobject SET name_loc9='Cristallo di Comunicazione' WHERE entry=210332; -- Name: Communication Crystal
+UPDATE locales_gameobject SET name_loc9='Gong di Ahn\'Qiraj' WHERE entry=210334; -- Name: Ahn'Qiraj Gong
+UPDATE locales_gameobject SET name_loc9='Tavoletta Twilight' WHERE entry=210335; -- Name: Twilight Tablet
+UPDATE locales_gameobject SET name_loc9='Tavoletta Twilight' WHERE entry=210336; -- Name: Twilight Tablet
+UPDATE locales_gameobject SET name_loc9='Cristallo Glifico' WHERE entry=210337; -- Name: Glyphed Crystal
+UPDATE locales_gameobject SET name_loc9='Cristallo Silithid Gorishi' WHERE entry=210338; -- Name: Gorishi Silithid Crystal
+UPDATE locales_gameobject SET name_loc9='Trappola Sabbiosa di Ahn\'Qiraj' WHERE entry=210339; -- Name: Ahn'Qiraj Sand Trap
+UPDATE locales_gameobject SET name_loc9='Nido di Lucciole 01' WHERE entry=210341; -- Name: Hive Fireflies 01
+UPDATE locales_gameobject SET name_loc9='Cristallo Glifico' WHERE entry=210342; -- Name: Glyphed Crystal
+UPDATE locales_gameobject SET name_loc9='Base Rocciosa Verme delle Sabbie' WHERE entry=210343; -- Name: Sand Worm Rock Base
+UPDATE locales_gameobject SET name_loc9='Formazione di Cristallo del Silithus 03' WHERE entry=210344; -- Name: Silithus Crystal Formation 03
+UPDATE locales_gameobject SET name_loc9='Cristallo Rosso Rotto Fluttuante 03' WHERE entry=210345; -- Name: Floating Red Crystal Broken 03
+UPDATE locales_gameobject SET name_loc9='Cristallo Rosso Rotto Fluttuante 01' WHERE entry=210346; -- Name: Floating Red Crystal Broken 01
+UPDATE locales_gameobject SET name_loc9='Porta di Ahn\'Qiraj 01' WHERE entry=210347; -- Name: Ahn'Qiraj Door 01
+UPDATE locales_gameobject SET name_loc9='Cristallo Viola Rotto Fluttuante 01' WHERE entry=210348; -- Name: Floating Purple Crystal Broken 01
+UPDATE locales_gameobject SET name_loc9='Uscita Gastrica' WHERE entry=210349; -- Name: Gastric Exit
+UPDATE locales_gameobject SET name_loc9='Incudine' WHERE entry=211016; -- Name: Anvil
+UPDATE locales_gameobject SET name_loc9='Vagone di Kre\'tal' WHERE entry=211017; -- Name: Kre'tal's Wagon
+UPDATE locales_gameobject SET name_loc9='Tomo dell\'Evocazione di Asheron' WHERE entry=211018; -- Name: Asheron's Tome of Summoning
+UPDATE locales_gameobject SET name_loc9='Altare di Hyjal' WHERE entry=211019; -- Name: Altar of Hyjal
+UPDATE locales_gameobject SET name_loc9='Circolo d\'Evocazione' WHERE entry=211020; -- Name: Summoning Circle
+UPDATE locales_gameobject SET name_loc9='Occhio di  Asheron' WHERE entry=211021; -- Name: Eye of Asheron
+UPDATE locales_gameobject SET name_loc9='Tenda del Comandante' WHERE entry=211022; -- Name: Commander's Tent
+UPDATE locales_gameobject SET name_loc9='Portale Nero' WHERE entry=211023; -- Name: Black Portal
+UPDATE locales_gameobject SET name_loc9='Portale Nero' WHERE entry=211024; -- Name: Black Portal
+UPDATE locales_gameobject SET name_loc9='Stendardo' WHERE entry=211029; -- Name: Banner
+UPDATE locales_gameobject SET name_loc9='Miniera d\'Oro' WHERE entry=211032; -- Name: Gold Mine
+UPDATE locales_gameobject SET name_loc9='Aura' WHERE entry=211033; -- Name: Aura
+UPDATE locales_gameobject SET name_loc9='Aura' WHERE entry=211034; -- Name: Aura
+UPDATE locales_gameobject SET name_loc9='Uscita del Covo' WHERE entry=211035; -- Name: Lair Exit
+UPDATE locales_gameobject SET name_loc9='Totem del Fuoco' WHERE entry=211036; -- Name: Fire Totem
+UPDATE locales_gameobject SET name_loc9='Pietra-Teletrasporto per Hyjal' WHERE entry=211050; -- Name: Teleport-Stone to Hyjal
+UPDATE locales_gameobject SET name_loc9='Ceppo d\'Albero Misterioso' WHERE entry=211051; -- Name: Mysterious Tree Stump
+UPDATE locales_gameobject SET name_loc9='Ceppo d\'Albero' WHERE entry=211052; -- Name: Tree Stump
+UPDATE locales_gameobject SET name_loc9='Monumento Misterioso' WHERE entry=211053; -- Name: Mysterious Monument
+UPDATE locales_gameobject SET name_loc9='Spaccatura Lavica' WHERE entry=211054; -- Name: Lava Crack
+UPDATE locales_gameobject SET name_loc9='Recinto Elwynn' WHERE entry=211062; -- Name: Elwynn Fence
+UPDATE locales_gameobject SET name_loc9='Recinto Elwynn' WHERE entry=211063; -- Name: Elwynn Fence
+UPDATE locales_gameobject SET name_loc9='Recinto Rockwall' WHERE entry=211064; -- Name: Rockwall Fence
+UPDATE locales_gameobject SET name_loc9='Tomba' WHERE entry=211065; -- Name: Grave
+UPDATE locales_gameobject SET name_loc9='Sarcofago' WHERE entry=211067; -- Name: Sarcophag
+UPDATE locales_gameobject SET name_loc9='Totem del Fuoco' WHERE entry=211068; -- Name: Fire Totem
+UPDATE locales_gameobject SET name_loc9='Hellfire di Mephistroph' WHERE entry=211084; -- Name: Mephistroph's HellFire
+UPDATE locales_gameobject SET name_loc9='TEMP Officina' WHERE entry=300010; -- Name: TEMP Machine Shop
+UPDATE locales_gameobject SET name_loc9='Tuberi Superficiali' WHERE entry=300011; -- Name: Nearby Tubers
+UPDATE locales_gameobject SET name_loc9='TEMP Pietra del Vincolo Esterno' WHERE entry=300012; -- Name: TEMP Stone of Outer Binding
+UPDATE locales_gameobject SET name_loc9='TEMP Disturbo nella Spaccatura del Mana' WHERE entry=300013; -- Name: TEMP Mana Rift Disturbance
+UPDATE locales_gameobject SET name_loc9='TEMP Purezza dell\'Acqua - Silverpine' WHERE entry=300014; -- Name: TEMP Water Purity - Silverpine
+UPDATE locales_gameobject SET name_loc9='TEMP Sentina della Nava Pirata' WHERE entry=300015; -- Name: TEMP Pirate Ship Bilge
+UPDATE locales_gameobject SET name_loc9='TEMP Villaggio Witherbark' WHERE entry=300016; -- Name: TEMP Witherbark Village
+UPDATE locales_gameobject SET name_loc9='TEMP Altare di Shadra\'Alor' WHERE entry=300017; -- Name: TEMP Shadra'Alor Altar
+UPDATE locales_gameobject SET name_loc9='TEMP Pietre Hatetalon' WHERE entry=300018; -- Name: TEMP Hatetalon Stones
+UPDATE locales_gameobject SET name_loc9='TEMP Luogo Sacro del Dio Caduto' WHERE entry=300019; -- Name: TEMP Sanctum of the Fallen God
+UPDATE locales_gameobject SET name_loc9='TEMP Miblon Snarltooth' WHERE entry=300020; -- Name: TEMP Miblon Snarltooth
+UPDATE locales_gameobject SET name_loc9='TEMP Cimitero di Gadgetzan' WHERE entry=300021; -- Name: TEMP Gadgetzan Graveyard
+UPDATE locales_gameobject SET name_loc9='TEMP le rovine dei Boschi Irontree' WHERE entry=300022; -- Name: TEMP the ruins of Irontree Woods
+UPDATE locales_gameobject SET name_loc9='TEMP Cratere Golakka' WHERE entry=300023; -- Name: TEMP Golakka Crater
+UPDATE locales_gameobject SET name_loc9='TEMP NIU Tomba dei Sette' WHERE entry=300024; -- Name: TEMP NIU Tomb of the Seven
+UPDATE locales_gameobject SET name_loc9='TEMP Monweel Corrotto' WHERE entry=300025; -- Name: TEMP Corrupted Moonwell
+UPDATE locales_gameobject SET name_loc9='TEMP Falò Settentrionale dei Fulborg Blackwood' WHERE entry=300026; -- Name: TEMP Blackwood Furbolg North Bonfire
+UPDATE locales_gameobject SET name_loc9='TEMP Pietra di Shy-Rotam' WHERE entry=300027; -- Name: TEMP Stone of Shy-Rotam
+UPDATE locales_gameobject SET name_loc9='TEMP Fuoco Sacro della Vita' WHERE entry=300028; -- Name: TEMP Sacred Fire of Life
+UPDATE locales_gameobject SET name_loc9='TEMP Avamposto della Scarlet Crusade' WHERE entry=300029; -- Name: TEMP Scarlet Crusade Forward Camp
+UPDATE locales_gameobject SET name_loc9='TEMP Torre di Andorhal' WHERE entry=300030; -- Name: TEMP Andorhal Tower
+UPDATE locales_gameobject SET name_loc9='TEMP Osservatorio di Scholomance' WHERE entry=300031; -- Name: TEMP Scholomance Viewing Room
+UPDATE locales_gameobject SET name_loc9='TEMP Amico di Umi' WHERE entry=300032; -- Name: TEMP Umi's Friend
+UPDATE locales_gameobject SET name_loc9='TEMP la cassa al centro della Segheria di Northridge' WHERE entry=300033; -- Name: TEMP the crate in the center of the Northridge Lumber Mill
+UPDATE locales_gameobject SET name_loc9='TEMP Piazza della Città di Darrowshire' WHERE entry=300034; -- Name: TEMP Darrowshire Town Square
+UPDATE locales_gameobject SET name_loc9='TEMP NIU Raggio di Luce Luminosa' WHERE entry=300035; -- Name: TEMP NIU Bright Light Beam
+UPDATE locales_gameobject SET name_loc9='TEMP I Golia Morti' WHERE entry=300036; -- Name: TEMP The Dead Goliaths
+UPDATE locales_gameobject SET name_loc9='TEMP Portale per Maraudon' WHERE entry=300037; -- Name: TEMP Maraudon Portal
+UPDATE locales_gameobject SET name_loc9='TEMP NIU Globo Cristalloscopico dell\'Orda' WHERE entry=300038; -- Name: TEMP NIU Horde Globe of Scrying
+UPDATE locales_gameobject SET name_loc9='TEMP Cratere Orientale' WHERE entry=300039; -- Name: TEMP Eastern Crater
+UPDATE locales_gameobject SET name_loc9='TEMP Cratere Occidentale' WHERE entry=300040; -- Name: TEMP Western Crater
+UPDATE locales_gameobject SET name_loc9='TEMP Cimitero Snowfall' WHERE entry=300041; -- Name: TEMP Snowfall Graveyard
+UPDATE locales_gameobject SET name_loc9='TEMP Globo Cristalloscopico dell\'Alleanza' WHERE entry=300042; -- Name: TEMP Alliance Globe of Scrying
+UPDATE locales_gameobject SET name_loc9='TEMP Dun Baldar Courtyard' WHERE entry=300043; -- Name: TEMP Dun Baldar Courtyard
+UPDATE locales_gameobject SET name_loc9='TEMP Cortile Fortezza Frostwolf' WHERE entry=300044; -- Name: TEMP Frostwolf Keep Courtyard
+UPDATE locales_gameobject SET name_loc9='TEMP Cimitero Snowfall' WHERE entry=300045; -- Name: TEMP Snowfall Graveyard
+UPDATE locales_gameobject SET name_loc9='TEMP Cratere Orientale' WHERE entry=300046; -- Name: TEMP East Crater
+UPDATE locales_gameobject SET name_loc9='TEMP Cratere Occidentale' WHERE entry=300047; -- Name: TEMP West Crater
+UPDATE locales_gameobject SET name_loc9='TEMP Pozza del Cristallo Arancione' WHERE entry=300048; -- Name: TEMP Orange Crystal Pool
+UPDATE locales_gameobject SET name_loc9='TEMP Resti di Kroshius' WHERE entry=300049; -- Name: TEMP Kroshius' Remains
+UPDATE locales_gameobject SET name_loc9='TEMP Piedistallo di Immol\'thar' WHERE entry=300050; -- Name: TEMP Pedestal of Immol'thar
+UPDATE locales_gameobject SET name_loc9='TEMP Circolo dell\'Evocazione Oscura' WHERE entry=300051; -- Name: TEMP Circle of Dark Summoning
+UPDATE locales_gameobject SET name_loc9='TEMP Pagle\'s Pointe' WHERE entry=300052; -- Name: TEMP Pagle's Pointe
+UPDATE locales_gameobject SET name_loc9='TEMP Altare di Zanza' WHERE entry=300053; -- Name: TEMP Altar of Zanza
+UPDATE locales_gameobject SET name_loc9='TEMP Southshore' WHERE entry=300054; -- Name: TEMP Southshore
+UPDATE locales_gameobject SET name_loc9='TEMP Ossa di Grakkarond' WHERE entry=300055; -- Name: TEMP Bones of Grakkarond
+UPDATE locales_gameobject SET name_loc9='TEMP Bomba Fetida dei Forsaken' WHERE entry=300056; -- Name: TEMP Forsaken Stink Bomb
+UPDATE locales_gameobject SET name_loc9='TEMP Maelstrom Vorticoso' WHERE entry=300057; -- Name: TEMP Swirling Maelstrom
+UPDATE locales_gameobject SET name_loc9='TEMP Moonlight Superiore' WHERE entry=300058; -- Name: TEMP Greater Moonlight
+UPDATE locales_gameobject SET name_loc9='TEMP Camera di Voone' WHERE entry=300059; -- Name: TEMP Voone's Chamber
+UPDATE locales_gameobject SET name_loc9='TEMP Camera di Alzzin' WHERE entry=300060; -- Name: TEMP Alzzin's Chamber
+UPDATE locales_gameobject SET name_loc9='TEMP Il Trono Cremisi' WHERE entry=300061; -- Name: TEMP The Crimson Throne
+UPDATE locales_gameobject SET name_loc9='TEMP Camera di Ras Frostwhisper' WHERE entry=300062; -- Name: TEMP Ras Frostwhisper's Chamber
+UPDATE locales_gameobject SET name_loc9='TEMP Camera Della Bestia' WHERE entry=300063; -- Name: TEMP The Beast's Chamber
+UPDATE locales_gameobject SET name_loc9='TEMP Loculo Infestato' WHERE entry=300064; -- Name: TEMP Haunted Locus
+UPDATE locales_gameobject SET name_loc9='TEMP Arena Blackrock Depths' WHERE entry=300065; -- Name: TEMP Blackrock Depths Arena
+UPDATE locales_gameobject SET name_loc9='TEMP [PH] Cadavere di Cristallo' WHERE entry=300066; -- Name: TEMP [PH] Crystal Corpse
+UPDATE locales_gameobject SET name_loc9='TEMP Falò Midsummer' WHERE entry=300068; -- Name: TEMP Midsummer Bonfire
+UPDATE locales_gameobject SET name_loc9='TEMP Sorgente d\'Acqua Sotterraneat' WHERE entry=300078; -- Name: TEMP Underground Water Source
+UPDATE locales_gameobject SET name_loc9='TEMP Roccia Piatta Un\'Goro' WHERE entry=300119; -- Name: TEMP Un'Goro Flat Rock
+UPDATE locales_gameobject SET name_loc9='TEMP Cumulo Totem Foulweald' WHERE entry=300131; -- Name: TEMP Foulweald Totem Mound
+UPDATE locales_gameobject SET name_loc9='TEMP Cascata Fiume Cliffspring' WHERE entry=300132; -- Name: TEMP Cliffspring River Waterfall
+UPDATE locales_gameobject SET name_loc9='TEMP Prima Pozza delle Maree' WHERE entry=300133; -- Name: TEMP First Tide Pool
+UPDATE locales_gameobject SET name_loc9='TEMP Seconda Pozza delle Maree' WHERE entry=300134; -- Name: TEMP Second Tide Pool
+UPDATE locales_gameobject SET name_loc9='TEMP Terza Pozza delle Maree' WHERE entry=300135; -- Name: TEMP Third Tide Pool
+UPDATE locales_gameobject SET name_loc9='TEMP Quarta Pozza delle Maree' WHERE entry=300136; -- Name: TEMP Fourth Tide Pool
+UPDATE locales_gameobject SET name_loc9='TEMP Altare di Jintha\'Alor' WHERE entry=300137; -- Name: TEMP Jintha'Alor Altar
+UPDATE locales_gameobject SET name_loc9='TEMP Monolite Equinex' WHERE entry=300138; -- Name: TEMP Equinex Monolith
+UPDATE locales_gameobject SET name_loc9='TEMP Frammenti Myzrael' WHERE entry=300139; -- Name: TEMP Shards of Myzrael
+UPDATE locales_gameobject SET name_loc9='TEMP Monumento di Grom' WHERE entry=300140; -- Name: TEMP Grom's Monument
+UPDATE locales_gameobject SET name_loc9='TEMP Grotta dell\'Alto Capitano Winterfall' WHERE entry=300141; -- Name: TEMP High Chief Winterfall's Cave
+UPDATE locales_gameobject SET name_loc9='TEMP Santuario di Remulos' WHERE entry=300142; -- Name: TEMP Shrine of Remulos
+UPDATE locales_gameobject SET name_loc9='TEMP Attracco della Villa Swamplight' WHERE entry=300143; -- Name: TEMP Swamplight Manor Dock
+UPDATE locales_gameobject SET name_loc9='TEMP Il Grande Ossario' WHERE entry=300144; -- Name: TEMP The Great Ossuary
+UPDATE locales_gameobject SET name_loc9='TEMP Statua della Tomba di Uther' WHERE entry=300145; -- Name: TEMP Uther's Tomb Statue
+UPDATE locales_gameobject SET name_loc9='TEMP Abbeveratoio Quilboar' WHERE entry=300146; -- Name: TEMP Quilboar Watering Hole
+UPDATE locales_gameobject SET name_loc9='TEMP Pozzo Zampillante' WHERE entry=300147; -- Name: TEMP Spring Well
+UPDATE locales_gameobject SET name_loc9='TEMP Rovine della Fontana Stardust' WHERE entry=300148; -- Name: TEMP Ruins of Stardust Fountain
+UPDATE locales_gameobject SET name_loc9='TEMP Mulino a Vento del Villaggio Blackhoof' WHERE entry=300149; -- Name: TEMP Blackhoof Village Windmill
+UPDATE locales_gameobject SET name_loc9='TEMP Tenda Grimtotem' WHERE entry=300150; -- Name: TEMP Grimtotem Tent
+UPDATE locales_gameobject SET name_loc9='TEMP Monumento Familiare Hyjal' WHERE entry=300151; -- Name: TEMP Hyjal Family Monument
+UPDATE locales_gameobject SET name_loc9='TEMP Entrata al Covo di Onyxia' WHERE entry=300153; -- Name: TEMP Entrance to Onyxia's Lair
 SET NAMES 'latin1';
