a34dea4eaffea89e458ee5b3667950417a568c18
Antz <billy1arm@users.noreply.github.com>
2018-02-13 22:59:55 +0000

    [Taiwanese] Merge Cleanup

--- Translations/Taiwanese/Taiwanese_Gossip_texts.sql
+++ Translations/Taiwanese/Taiwanese_Gossip_texts.sql
@@ -5,12 +5,14 @@ UPDATE gossip_texts SET content_loc5='讓溫德索爾元帥知道我準備好了
 UPDATE gossip_texts SET content_loc5='我準備好了，我的軍隊也整裝待命。讓我們結束這次化妝舞會吧!' WHERE entry=-3000107;
 UPDATE gossip_texts SET content_loc5='請留步，先生。' WHERE entry=-3000108;
 UPDATE gossip_texts SET content_loc5='我準備好了，歐朗諾克。讓我們毀了賽洛庫並解放那些元素吧!' WHERE entry=-3000109;
+UPDATE gossip_texts SET content_loc5='什麼...喔，當然。我要給你看的東西就在這棟大樓裡，安維爾伍德先生。' WHERE entry=-3000110;
 UPDATE gossip_texts SET content_loc5='轉動鑰匙，啟動了機器。' WHERE entry=-3000111;
 UPDATE gossip_texts SET content_loc5='請打開庭院的大門。' WHERE entry=-3033000;
 UPDATE gossip_texts SET content_loc5='活動開始了!' WHERE entry=-3043000;
 UPDATE gossip_texts SET content_loc5='我準備好了。' WHERE entry=-3090000;
 UPDATE gossip_texts SET content_loc5='你自由了，杜格哈爾!快出去!' WHERE entry=-3230000;
 UPDATE gossip_texts SET content_loc5='出去吧，托比亞斯，你自由了!' WHERE entry=-3230001;
+UPDATE gossip_texts SET content_loc5='你的奴役結束了，杜姆雷爾。我向你挑戰!' WHERE entry=-3230002;
 UPDATE gossip_texts SET content_loc5='為什麼你跟羅克諾特不去找個二人天地呢...' WHERE entry=-3230003;
 UPDATE gossip_texts SET content_loc5='告訴我更多的資訊。' WHERE entry=-3409000;
 UPDATE gossip_texts SET content_loc5='你還想說什麼?' WHERE entry=-3409001;
